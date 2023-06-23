85c3e526c5aff4a10df2cd2afd22d585210ed97f
DavidM0208 <sdcifuentes85@misena.edu.co>
2023-06-22 21:00:38 -0500

    Enlace con la base de datos usando WAMP

--- controllers/login.php
+++ controllers/login.php
@@ -4,6 +4,6 @@ include_once "../models/conexion.php";
 $username=$_POST['username'];
 $password=$_POST['password'];
 
-print_r($username);die;
+
 
 ?>
--- models/conexion.php
+++ models/conexion.php
@@ -1,5 +1,5 @@
 <?php 
-$mysqli = new mysqli('localhost', 'root', '123456', 'sencoe');
+$mysqli = new mysqli("localhost", "root", "", "sencoe");
 if($mysqli->connect_errno){
     echo "Fallo al conectar a mysql: (".$mysqli->connect_errno.")".$mysqli->connect_error;
 }
